7d40787fa97d3ea872acefd861ce57d0495af026
francislu2021 <85589987+francislu2021@users.noreply.github.com>
2021-09-24 11:01:53 -0400

    search enable

--- index.js
+++ index.js
@@ -80,3 +80,11 @@ function GetLatlong() {
         }
     });
 }
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
